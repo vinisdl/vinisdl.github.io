a1e4f6c1952a204a74e5615123711adbf602a3cf
Marco Dalalba <vinisdl@hotmail.com>
2016-07-13 10:38:16 -0300

    create categorys a for home and posts, remove trash from js

--- js/custom.js
+++ js/custom.js
@@ -82,14 +82,14 @@ $(document).ready(function() {
 		});
 	}
 	
-	if($(window).width() > 767) {
+	/*if($(window).width() > 767) {
 		$('.scrollpoint.sp-effect1').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInLeft');},{offset:'90%'});
 		$('.scrollpoint.sp-effect2').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInRight');},{offset:'90%'});
 		$('.scrollpoint.sp-effect3').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeInDown');},{offset:'90%'});
 		$('.scrollpoint.sp-effect4').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('animated fadeIn');},{offset:'60%'});
 		
 		$('.macbook-inner').waypoint(function(){$(this).toggleClass('active');$(this).toggleClass('black');},{offset:'60%'});
-	}
+	}*/
 });
 
 
